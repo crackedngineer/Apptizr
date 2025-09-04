3f2e59128406a301bd4b08cbb74de608f289cc44
Subhomoy Roy Choudhury <64763636+crackedngineer@users.noreply.github.com>
2025-09-04 09:59:29 +0000

    integrated docker in web

--- web/next.config.ts
+++ web/next.config.ts
@@ -7,5 +7,12 @@ const nextConfig: NextConfig = {
 export default nextConfig;
 
 // added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
-initOpenNextCloudflareForDev();
+
+if (process.env.NEXT_PUBLIC_DEPLOYMENT_PLATFORM !== "docker") {
+  import('@opennextjs/cloudflare').then(({ initOpenNextCloudflareForDev }) => {
+    initOpenNextCloudflareForDev();
+  }).catch((err) => {
+    console.error("Failed to initialize OpenNext Cloudflare:", err);
+  });
+}
+
